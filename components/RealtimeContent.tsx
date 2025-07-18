6c199a138c534ae36339e323524e3d7815acb194
gayoung <gayoung990911@gmail.com>
2025-07-18 17:20:34 +0900

    Fix TypeScript Error

--- components/RealtimeContent.tsx
+++ components/RealtimeContent.tsx
@@ -699,7 +699,8 @@ export default function RealtimeContent() {
                                       {duration.toFixed(0)}ms
                                     </span>
                                     {isError && (
-                                      <svg className="w-3 h-3 text-red-500" fill="currentColor" viewBox="0 0 20 20" title={`Status: ${actualStatus}`}>
+                                      <svg className="w-3 h-3 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+                                        <title>Status: {actualStatus}</title>
                                         <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                                       </svg>
                                     )}
